79534470d966060148d750e099a911bdb2d11ee5
Benny Snatch <91598069+BennySnatch@users.noreply.github.com>
2022-07-16 00:53:15 +0100

    title text change

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>NextJs UI kit | Free UI kit built with bootstrap</title>
+        <title>Hamster Heroes of Draebanor | Punch you in the tits</title>
         <meta
           name="description"
-          content="NextJs UI kit | Free UI kit built with bootstrap"
+          content="Hamster Heroes of Draebanor | Punch you in the tits"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
